31d5647dbd81526af8fffa935452c385e6e218bb
brilajn <brilajn@gmail.com>
2022-04-14 17:53:49 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+//console.log(1);
+//let number = 5;
+//const LeftBorder = 3;
+//{
+//    var number = 2;
+//}
+//console.log(number);
+//alert("7");
+//let result = confirm ("Are you ok?");
+//console.log(result);
+//const answer = prompt("Сколько вам лет?","28");
+//console.log(answer);
+//console.log(typeof(answer));
+const answers = [];
+answers[0]=prompt("Ваше имя?","");
+answers[1]=prompt("Ваша фамилия?","");
+answers[2]=prompt("Сколько Вам лет?","");
+document.write(answers);
+document.write(typeof(answers));
