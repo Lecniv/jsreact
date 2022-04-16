ea91bb071bdabc88ba30a03909bafb5eae34c38f
brilajn <brilajn@gmail.com>
2022-04-16 12:42:19 +0300

    lesson 19

--- js/script.js
+++ js/script.js
@@ -12,9 +12,25 @@
 //const answer = prompt("Сколько вам лет?","28");
 //console.log(answer);
 //console.log(typeof(answer));
-const answers = [];
-answers[0]=prompt("Ваше имя?","");
-answers[1]=prompt("Ваша фамилия?","");
-answers[2]=prompt("Сколько Вам лет?","");
-document.write(answers);
-document.write(typeof(answers));
+//const answers = [];
+//answers[0]=prompt("Ваше имя?","");
+//answers[1]=prompt("Ваша фамилия?","");
+//answers[2]=prompt("Сколько Вам лет?","");
+//document.write(answers);
+//document.write(typeof(answers));
+let numberOfFilms = prompt("сколько фильмов вы уже просмотрели?","");
+let personalMouvieDb ={
+    count: numberOfFilms,
+    mouvies: {},    //пустой объект
+    actors: {},
+    genres: [],     //пустой массив
+    privat: false
+};
+const   a = prompt("Название фильма", ""),
+        b = prompt("оценка", ""),
+        c = prompt("Название фильма", ""),
+        d = prompt("оценка", "");
+        personalMouvieDb.mouvies[a]=b;
+        personalMouvieDb.mouvies[c]=d;
+//через (.) в консоли не показывает 
+        console.log(personalMouvieDb);
